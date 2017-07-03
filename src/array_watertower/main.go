658100851b1b277a3d8baf565b0d8691d6a89143
marshall king <marshallking@marshalls-MBP.domain>
2017-07-03 14:17:51 -0700

    sync

--- src/array_watertower/main.go
+++ src/array_watertower/main.go
@@ -10,6 +10,7 @@ import (
 
 const APP_VERSION = "0.1"
 
+
 // The flag package provides a default help printer via -h switch
 var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
 
